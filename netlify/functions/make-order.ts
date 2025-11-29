d1f8efec1170ef4498a8d83b536eafe6b20188ec
xfranklin <git.franklin@gmail.com>
2025-11-29 20:04:38 +0200

    feat: make-order.ts

--- netlify/functions/make-order.ts
+++ netlify/functions/make-order.ts
@@ -7,37 +7,47 @@ const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const CHAT_ID = process.env.CHAT_ID;
 
 const getMessage = (name: string, phone: string, isLowRating: boolean = false) => {
-  return `New site order: \n👤 Имя: ${name ?? "-"} \n📧 Телефон: ${phone ?? "-"} \n ${isLowRating ? "low order rating" : ""}`.trim();
+  return `New site order: \n👤 Name: ${name ?? "-"} \n📧 Phone: ${phone ?? "-"} \n ${isLowRating ? "low order rating" : ""}`.trim();
 };
 
 export default async (req: Request, context: Context) => {
-  if (!req.body) return;
+  try {
+    if (!req.body) return;
 
-  const bodyText = await req.text();
-  const body = JSON.parse(bodyText || "{}");
+    const bodyText = await req.text();
+    const body = JSON.parse(bodyText || "{}");
 
-  const googleRes = await fetch(CAPTCHA_VERIFY_URL, {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams({
-      secret: CAPTCHA_TOKEN,
-      response: body.token
-    })
-  });
-  const data = await googleRes.json();
+    const googleRes = await fetch(CAPTCHA_VERIFY_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams({
+        secret: CAPTCHA_TOKEN,
+        response: body.token
+      })
+    });
 
-  const tgResponse = await fetch(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      chat_id: CHAT_ID,
-      text: getMessage(body.name, body.phone, data.score <= 0.4),
-      parse_mode: "HTML"
-    })
-  });
+    const data = await googleRes.json();
+    if (data.success !== true) throw new Error("Captcha error");
 
-  return new Response(JSON.stringify(data), {
-    status: 200,
-    headers: { "content-type": "application/json" }
-  });
+    const tgResponse = await fetch(
+      `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          chat_id: CHAT_ID,
+          text: getMessage(body.name, body.phone, data.score <= 0.4),
+          parse_mode: "HTML"
+        })
+      }
+    );
+
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: { "content-type": "application/json" }
+    });
+  } catch (e) {
+    console.error(e);
+    return new Response(e.message, { status: 500 });
+  }
 };
